d6ac8f80e53ca51aa49b2169290a259d4dbbcc58
Sma Cohen <cohen.samuel@wustl.edu>
2020-12-08 15:10:53 -0500

    added large videos

--- translate_numpy.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import os
-import numpy as np
-import re
-
-
-x_train, y_train, x_test = np.load("./data/x_train.npy"), np.load("./data/y_train.npy"), np.load("./data/x_test.npy")
-y_train = np.array([float(j) for j in y_train])
-print('hey')
